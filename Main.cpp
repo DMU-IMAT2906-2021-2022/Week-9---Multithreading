edf2f7887fe26cfb7cb03a828c1e71a156e5993e
jethros <jethros@dmu.ac.uk>
2022-03-14 12:00:16 +0000

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,7 @@
+//**Main to be used to process the files supplied whilst allowing for the input of numbers**//
+#include<iostream>
+
+int main(){
+	std::cout << "Multithreading" << std::endl;
+	return 0;
+}
